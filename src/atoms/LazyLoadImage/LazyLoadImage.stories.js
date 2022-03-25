5095636a6df9d880256427c6e24398858a43bd21
Joey Imlay <jo.imlay@gmail.com>
2022-03-25 16:10:51 +0000

    updates LazyLoadImage story

--- src/atoms/LazyLoadImage/LazyLoadImage.stories.js
+++ src/atoms/LazyLoadImage/LazyLoadImage.stories.js
@@ -4,15 +4,56 @@ import LazyLoadImage from '.';
 export default {
   title: 'molecules/LazyLoadImage',
   component: LazyLoadImage,
+  argTypes: {
+    src: {
+      control: {
+        type: 'text'
+      }
+    },
+    alt: {
+      control: {
+        type: 'object'
+      }
+    },
+    defaultWidthPx: {
+      control: {
+        type: 'number'
+      }
+    },
+    sizes: {
+      control: {
+        type: 'object'
+      }
+    },
+    srcsetSizes: {
+      control: {
+        type: 'array'
+      }
+    },
+    widthPx: {
+      control: {
+        type: 'number'
+      }
+    },
+  }
 };
 
-const Template = () => (
+const Template = ({ src, alt, defaultWidthPx, sizes, srcsetSizes, widthPx }) => (
   <LazyLoadImage
-    src="https://ik.imagekit.io/surinderbhomra/Pages/logo-me.jpg"
-    widthPx={400}
-    srcsetSizes={[{ imageWidth: 400, viewPortWidth: 992 }, { imageWidth: 300, viewPortWidth: 768 }, { imageWidth: 200, viewPortWidth: 500 }]}
-    alt="Surinder Logo"
+    src={src}
+    widthPx={widthPx}
+    srcsetSizes={srcsetSizes}
+    alt={alt}
+    defaultWidthPx={defaultWidthPx}
+    sizes={sizes}
   />
 );
 
 export const Default = Template.bind({});
+
+Default.args = {
+  src: "http://placekitten.com/400/400",
+  widthPx: 400,
+  srcsetSizes: [{ imageWidth: 400, viewPortWidth: 992 }, { imageWidth: 300, viewPortWidth: 768 }, { imageWidth: 200, viewPortWidth: 500 }],
+  alt: "A cat from placekitten.com"
+}
